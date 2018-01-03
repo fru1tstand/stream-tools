d2086f7ab137393f6af718b546da2dea69023ded
kodlee yin <kodlee@fru1t.me>
2018-01-02 21:25:56 -0800

    Add test placeholder
    Added test placeholders for Controller/ControllerTest and test
    resources.

--- src/test/kotlin/me/fru1t/javafx/ControllerTest.kt
+++ src/test/kotlin/me/fru1t/javafx/ControllerTest.kt
@@ -1,14 +1,22 @@
 package me.fru1t.javafx
 
 import com.google.common.truth.Truth.assertThat
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
 @RunWith(JUnit4::class)
 class ControllerTest {
+  private lateinit var testController: Controller
+
+  @Before
+  fun setup() {
+    testController = FakeController()
+  }
+
   @Test
   fun getFxmlResourcePath() {
-    assertThat(true).isTrue()
+    assertThat(testController.getFxmlResourcePath()).isEqualTo("/FXML/test_fxml.fxml")
   }
 }
---
+++ src/test/kotlin/me/fru1t/javafx/FakeController.kt
@@ -0,0 +1,5 @@
+package me.fru1t.javafx
+
+/** An empty implementation of Controller for testing. */
+@FxmlResource("/FXML/test_fxml.fxml")
+class FakeController : Controller()
