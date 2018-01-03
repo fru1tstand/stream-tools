ed2eca6f768d2592e521502e306a8ced15f46ab3
kodlee yin <kodlee@fru1t.me>
2018-01-02 21:10:08 -0800

    Add tests to stream-tools module
    Unit tests with JUnit4, Google Truth, and Mockito.

---
+++ src/test/kotlin/me/fru1t/javafx/ControllerTest.kt
@@ -0,0 +1,14 @@
+package me.fru1t.javafx
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class ControllerTest {
+  @Test
+  fun getFxmlResourcePath() {
+    assertThat(false).isTrue()
+  }
+}
